ac5d645b17ca2a15f2354d119cdd18ecd25f9e56
Your Name <you@example.com>
2025-09-19 00:57:53 +0800

    cicd

--- plugin.go
+++ plugin.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm"
 	"github.com/higress-group/proxy-wasm-go-sdk/proxywasm/types"
@@ -14,15 +16,15 @@ import (
 
 func main() {
 
-	wrapper.SetCtx(
-		// 插件名称
-		"my-pluginsss",
-		// 为解析插件配置，设置自定义函数
-		wrapper.ParseConfigBy(parseConfig),
-		// 为处理请求头，设置自定义函数
-		wrapper.ProcessRequestHeadersBy(onHttpRequestHeaders),
-		wrapper.ProcessRequestBody(onHttpRequestBody),
-	)
+	// wrapper.SetCtx(
+	// 	// 插件名称
+	// 	"my-pluginsss",
+	// 	// 为解析插件配置，设置自定义函数
+	// 	wrapper.ParseConfigBy(parseConfig),
+	// 	// 为处理请求头，设置自定义函数
+	// 	wrapper.ProcessRequestHeadersBy(onHttpRequestHeaders),
+	// 	wrapper.ProcessRequestBody(onHttpRequestBody),
+	// )
 }
 
 func init() {
@@ -40,32 +42,39 @@ func init() {
 // 自定义插件配置
 type MyConfig struct {
 	mockEnable bool
-	actions    []map[string]interface{}
+	actions    []map[string]gjson.Result
 }
 
 // 在控制台插件配置中填写的yaml配置会自动转换为json，此处直接从json这个参数里解析配置即可
 func parseConfig(json gjson.Result, config *MyConfig, log logs.Log) error {
 	// 解析出配置，更新到config中
 	config.mockEnable = json.Get("mockEnable").Bool()
-	action1 := map[string]any{
-		"field": []string{"name", "email"},
-		"type":  "removeLeadingZero",
+	ac := json.Get("actions")
+	var actions []map[string]gjson.Result
+	for _, item := range ac.Array() {
+		actions = append(actions, item.Map())
 	}
-	action2 := map[string]any{
-		"field": map[string]int{
-			"username": 20,
-			"company":  30,
-		},
-		"type": "paddingZero",
-	}
-	action3 := map[string]any{
-		"field":   []string{"firstName", "lastName"},
-		"type":    "contact",
-		"newName": "fullName",
-	}
-	config.actions = []map[string]any{
-		action1, action2, action3,
-	}
+	config.actions = actions
+	/*
+		action1 := map[string]any{
+			"field": []string{"name", "email"},
+			"type":  "removeLeadingZero",
+		}
+		action2 := map[string]any{
+			"field": map[string]int{
+				"username": 20,
+				"company":  30,
+			},
+			"type": "paddingZero",
+		}
+		action3 := map[string]any{
+			"field":   []string{"firstName", "lastName"},
+			"type":    "contact",
+			"newName": "fullName",
+		}
+		config.actions = []map[string]any{
+			action1, action2, action3,
+		}*/
 	return nil
 }
 
@@ -141,4 +150,23 @@ func concatField(datas map[string]interface{}, fields []string, newName string, 
 	}
 	return datas
 
+}
+
+func paddingZero(datas map[string]interface{}, fields []string, zeroCount int) map[string]interface{} {
+	for _, val := range fields {
+		value, ok := datas[val]
+		if ok {
+			strval := fmt.Sprintf("%v", value)
+			length := len(strval) + zeroCount
+			floatValue, err := strconv.ParseFloat(strval, 64)
+			if err == nil {
+				s, _ := fmt.Printf("%*d", length, floatValue)
+				datas[val] = s
+			} else {
+				paddedStr := strings.Repeat("0", zeroCount) + strval
+				datas[val] = paddedStr
+			}
+		}
+	}
+	return datas
 }
