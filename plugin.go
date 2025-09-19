da181bcd03630bae4f295d2d75cfbf6217351500
unknown <kerby.xiao@clp.com.hk>
2025-09-19 18:02:07 +0800

    持续集成

--- plugin.go
+++ plugin.go
@@ -39,22 +39,72 @@ func init() {
 	)
 }
 
+type RemoveLeadingZero struct {
+	Fields    []string
+	Recursive bool
+}
+
+type PaddingZero struct {
+	Field     string
+	ZeroCount int
+	Recursive bool
+}
+
+type ConcatFields struct {
+	Fields       []string
+	NewFieldName string
+	Recursive    bool
+	KeepFields   bool
+}
+
 // 自定义插件配置
 type MyConfig struct {
-	mockEnable bool
-	actions    []map[string]gjson.Result
+	debug             bool
+	RemoveLeadingZero RemoveLeadingZero
+	PaddingZeros      []PaddingZero
+	ConcatFields      ConcatFields
 }
 
 // 在控制台插件配置中填写的yaml配置会自动转换为json，此处直接从json这个参数里解析配置即可
 func parseConfig(json gjson.Result, config *MyConfig, log logs.Log) error {
 	// 解析出配置，更新到config中
-	config.mockEnable = json.Get("mockEnable").Bool()
+	//config.mockEnable = json.Get("mockEnable").Bool()
 	ac := json.Get("actions")
-	var actions []map[string]gjson.Result
+	//var actions []map[string]gjson.Result
+	//myconf := MyConfig{}
 	for _, item := range ac.Array() {
-		actions = append(actions, item.Map())
+		action := item.Get("type").String()
+		if action == "removeLeadingZero" {
+			rmLeadingZero := RemoveLeadingZero{
+				Recursive: item.Get("recursive").Bool(),
+			}
+			if fields := item.Get("fields"); fields.Exists() && fields.IsArray() {
+				fields.ForEach(func(_, value gjson.Result) bool {
+					rmLeadingZero.Fields = append(rmLeadingZero.Fields, value.String())
+					return true
+				})
+			}
+			config.RemoveLeadingZero = rmLeadingZero
+		}
+
+		if action == "paddingZero" {
+			fieldMap := item.Get("fields")
+			if fieldMap.Exists() {
+				var paddingZeros []PaddingZero
+				fieldMap.ForEach(func(key, value gjson.Result) bool {
+					paddingZeros = append(paddingZeros, PaddingZero{Field: key.String(), ZeroCount: int(value.Int())})
+					return true
+				})
+				config.PaddingZeros = paddingZeros
+			}
+		}
+		//actions = append(actions, item.Map())
 	}
-	config.actions = actions
+
+	fmt.Println(config)
+
+	//fmt.Println("actions============================>", actions)
+	//config.actions = actions
 	/*
 		action1 := map[string]any{
 			"field": []string{"name", "email"},
@@ -80,9 +130,9 @@ func parseConfig(json gjson.Result, config *MyConfig, log logs.Log) error {
 
 func onHttpRequestHeaders(ctx wrapper.HttpContext, config MyConfig, log logs.Log) types.Action {
 	proxywasm.AddHttpRequestHeader("hello", "world")
-	if config.mockEnable {
-		proxywasm.SendHttpResponse(200, nil, []byte("hello world"), -1)
-	}
+	// if config.mockEnable {
+	// 	proxywasm.SendHttpResponse(200, nil, []byte("hello world"), -1)
+	// }
 	return types.HeaderContinue
 }
 
@@ -92,17 +142,24 @@ func onHttpRequestBody(ctx wrapper.HttpContext, config MyConfig, body []byte) ty
 	// for k, v := range result {
 	// 	//fmt.Println(k, "===>", v)
 	// }
-	updateNestedMap(result, "name", false)
+	//updateNestedMap(result, "name", false)
 
-	fmt.Println("updated result######", result)
-	for _, actionType := range config.actions {
-		fmt.Println(actionType["type"])
-		fmt.Println("fields", actionType["field"])
-	}
+	/*
+		fmt.Println("updated result######", result)
+		for _, actionType := range config.actions {
+			fmt.Println(actionType["type"])
+			fmt.Println("fields", actionType["field"])
+		}
 
-	concatField(result, []string{"firstName", "lastName"}, "fullName", false)
-	fmt.Println("after  concat result", result)
+		//concatField(result, []string{"firstName", "lastName"}, "fullName", false)
+		//fmt.Println("after  concat result", result)
+		paddingAction := config.actions[0]
+		for field, count := range paddingAction {
 
+		}
+		//paddingZero(result, )*/
+	pzr := paddingZero(result, config.PaddingZeros)
+	fmt.Println("padding Zero result########################", pzr)
 	return types.ActionContinue
 }
 
@@ -152,19 +209,26 @@ func concatField(datas map[string]interface{}, fields []string, newName string, 
 
 }
 
-func paddingZero(datas map[string]interface{}, fields []string, zeroCount int) map[string]interface{} {
-	for _, val := range fields {
-		value, ok := datas[val]
+func paddingZero(datas map[string]interface{}, fields []PaddingZero) map[string]interface{} {
+	fmt.Println("fields================================>", fields)
+	for _, obj := range fields {
+		value, ok := datas[obj.Field]
 		if ok {
+			fmt.Println("okkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
+			fmt.Println("value##################################", value)
 			strval := fmt.Sprintf("%v", value)
-			length := len(strval) + zeroCount
+			length := len(strval) + obj.ZeroCount
+			fmt.Println("lengtttttttttttttttttttttttttttttttttttttttt", length)
 			floatValue, err := strconv.ParseFloat(strval, 64)
+			fmt.Println("errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr", err)
+			fmt.Println("float value==================================>", floatValue)
 			if err == nil {
-				s, _ := fmt.Printf("%*d", length, floatValue)
-				datas[val] = s
+				s, _ := fmt.Printf("%0*d", length, floatValue)
+				fmt.Println("ssssssssssssssssssssssssss========================>", s)
+				datas[obj.Field] = s
 			} else {
-				paddedStr := strings.Repeat("0", zeroCount) + strval
-				datas[val] = paddedStr
+				paddedStr := strings.Repeat("0", obj.ZeroCount) + strval
+				datas[obj.Field] = paddedStr
 			}
 		}
 	}
